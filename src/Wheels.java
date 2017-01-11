68f42575acc2b230c6af630f8a8983403bd4e61d
MrOctantis <zmudd@san.rr.com>
2017-01-10 22:00:00 -0800

    Add Wheels.java
    In Skeleton State

---
+++ src/Wheels.java
@@ -0,0 +1,23 @@
+public class Wheels
+{
+	public void setMovement(double inOne, double inTwo)
+	{
+		// Take in joystick input, make the robot move.
+	}
+	public void stop()
+	{
+		// Make the robot stop moving
+	}
+	public void setTurbo()
+	{
+		// Toggle 'turbo' mode
+	}
+	public void setControls(int controlScheme)
+	{
+		// Set the control scheme to different presets depending on different driver preferences based on the variable passed
+	}
+	public int getControls()
+	{
+		// Return the int representation of the current control scheme
+	}
+}
