4a3f4e3cc3838c435e58682e33c1fcedc2621e43
MrOctantis <zmudd@san.rr.com>
2017-01-10 21:59:27 -0800

    Add Robot.java
    In Skeleton State

---
+++ src/Robot.java
@@ -0,0 +1,23 @@
+public class Robot extends InterativeRobot
+{
+	private void robotInit()
+	{
+		// Global initialization
+	}
+	private void autonomousInit()
+	{
+		// Initialize for autonomous specific tasks
+	}
+	private void autonomousPeriodic()
+	{
+		// Perform autonomous tasks
+	}
+	private void teleopInit()
+	{
+		// Initialize for teleop specific tasks
+	}
+	private void teleopPeriodic()
+	{
+		// Perform teleop tasks
+	}
+}
