8dd0d7b939fc54b722c6e1afa304515a805a0337
MrOctantis <zmudd@san.rr.com>
2017-01-10 22:00:55 -0800

    Add Motorcontrollers.java
    In Skeleton State

---
+++ src/MotorControllers.java
@@ -0,0 +1,11 @@
+public class MotorControllers
+{
+	public void doSomeAction()
+	{
+		// Run some predefined action
+	}
+	public void doSomeVariableAction(double value)
+	{
+		// Run some action based on the parameter passed
+	}
+}
