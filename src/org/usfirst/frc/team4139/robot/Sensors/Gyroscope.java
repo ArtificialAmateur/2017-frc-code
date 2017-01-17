2f773c50ecb9dac823a9c3f5bc0980e6083e3073
ArtificialAmateur <20297606+ArtificialAmateur@users.noreply.github.com>
2017-01-16 20:40:18 -0800

    Add Gyroscope.Java

--- src/org/usfirst/frc/team4139/robot/Sensors/Gyroscope.java
+++ src/org/usfirst/frc/team4139/robot/Sensors/Gyroscope.java
@@ -1,6 +1,25 @@
 package org.usfirst.frc.team4139.robot.Sensors;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+
 public class Gyroscope
 {
+	private Gyro mygyro;
 
+	public Gyroscope()
+	{
+		mygyro = new AnalogGyro(1);
+	}
+	
+	public void reset()
+	{
+		mygyro.reset();
+	}
+	
+	public double getangle()
+	{
+		double num = mygyro.getAngle();
+		return num;
+	}
 }
