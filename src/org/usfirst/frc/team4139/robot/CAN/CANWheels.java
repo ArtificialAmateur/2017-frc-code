7fd57041e860df223f41dac921ea6b235279e4b3
theparcivel <ryanmtannenberg@gmail.com>
2017-01-17 15:09:11 -0800

    heres ur damn wheels franko

--- src/org/usfirst/frc/team4139/robot/CAN/CANWheels.java
+++ src/org/usfirst/frc/team4139/robot/CAN/CANWheels.java
@@ -1,6 +1,43 @@
 package org.usfirst.frc.team4139.robot.CAN;
 
+import com.ctre.*;
+import edu.wpi.first.wpilibj.*;
+
 public class CANWheels
 {
-
+	public CANTalon frontLeftMotor = new CANTalon(0);
+	public CANTalon rearLeftMotor = new CANTalon(1);
+	public CANTalon frontRightMotor = new CANTalon(2);
+	public CANTalon rearRightMotor = new CANTalon(3);
+	
+	
+	public void setInverted(int motorvalue, boolean bool)
+	{
+		if(bool)
+		{
+			switch(motorvalue)
+			{
+			case 0: frontLeftMotor.setInverted(true);
+			break;
+			case 1: rearLeftMotor.setInverted(true);
+			break;
+			case 2: frontRightMotor.setInverted(true);
+			break;
+			case 3: rearRightMotor.setInverted(true);
+			}
+		}
+		else
+		{
+			switch(motorvalue)
+			{
+			case 0: frontLeftMotor.setInverted(false);
+			break;
+			case 1: rearLeftMotor.setInverted(false);
+			break;
+			case 2: frontRightMotor.setInverted(false);
+			break;
+			case 3: rearRightMotor.setInverted(false);
+			}
+		}
+	}
 }
