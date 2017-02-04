d7a35db11dd190ba98255ea637f9b235c0848ff5
Daniel <dritchie1031@gmail.com>
2017-02-04 11:35:20 -0800

    Teleop: Works. Autonomous: Still in testing

--- src/org/usfirst/frc/team4139/robot/Robot.java
+++ src/org/usfirst/frc/team4139/robot/Robot.java
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team4139.robot;
 
-import edu.wpi.first.wpilibj.*;
-import org.usfirst.frc.team4139.robot.CAN.*;
-import org.usfirst.frc.team4139.robot.PWM.*;
-import org.usfirst.frc.team4139.robot.Sensors.*;
+import java.util.Stack;
+
+import org.usfirst.frc.team4139.robot.CAN.CANWheels;
+import org.usfirst.frc.team4139.robot.Sensors.Controller;
+import org.usfirst.frc.team4139.robot.Utils.DriveInstruction;
+import org.usfirst.frc.team4139.robot.Utils.Instruction;
+import org.usfirst.frc.team4139.robot.Utils.NoInstruction;
+import org.usfirst.frc.team4139.robot.Utils.TurnDir;
+import org.usfirst.frc.team4139.robot.Utils.TurnInstruction;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -14,8 +21,11 @@ import org.usfirst.frc.team4139.robot.Sensors.*;
  */
 public class Robot extends IterativeRobot
 {
+	private Stack<Instruction> instructions;
 	// Initialize variables here
-	
+	private CANWheels wheels;
+	private Controller stick;
+	private Instruction currentInstruction;
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -29,7 +39,19 @@ public class Robot extends IterativeRobot
 	@Override
 	public void autonomousInit()
 	{
+		wheels = new CANWheels(2,3,4,1);
+		wheels.switchToArcade();
 		
+		currentInstruction = new NoInstruction();
+		
+		instructions = new Stack<Instruction>();
+		instructions.push(new DriveInstruction(wheels, 2));
+		/*instructions.push(new TurnInstruction(wheels, -90, TurnDir.right));
+		instructions.push(new DriveInstruction(wheels, 2));
+		instructions.push(new TurnInstruction(wheels, -90, TurnDir.right));
+		instructions.push(new DriveInstruction(wheels, 2));
+		instructions.push(new TurnInstruction(wheels, -90, TurnDir.right));
+		instructions.push(new DriveInstruction(wheels, 2));*/
 	}
 	
 	/**
@@ -38,13 +60,18 @@ public class Robot extends IterativeRobot
 	@Override
 	public void autonomousPeriodic()
 	{
-		
+		if(currentInstruction != null && currentInstruction.execute()){
+			currentInstruction = instructions.pop();
+		}
 	}
 	
 	@Override
 	public void teleopInit()
 	{
-		
+		wheels = new CANWheels(2,3,4,1);
+		wheels.switchToArcade();
+		stick = new Controller();
+		stick.setArcade();
 	}
 
 	/**
@@ -53,7 +80,7 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopPeriodic()
 	{
-		
+		wheels.drive(stick.getStickLeft(), stick.getStickRight());
 	}
 
 	@Override
