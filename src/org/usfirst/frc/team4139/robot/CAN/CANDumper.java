9301887aca060bd944a23c6ac4a1730a56c8bed3
dzhou <zhoudexin00@126.com>
2017-02-06 14:46:59 -0800

    Update CANDumper.java

--- src/org/usfirst/frc/team4139/robot/CAN/CANDumper.java
+++ src/org/usfirst/frc/team4139/robot/CAN/CANDumper.java
@@ -1,72 +1,93 @@
 //I DON'T KNOW WHAT CHANNELS TO PUT IN ANALOGOUTPUT. PLEASE FIX BEFORE RUNNING. THANK.
 //ALSO I DON'T KNOW WHAT NUMBER(S) THE SWITCH RETURNS. I'M ASSUMING ZERO MEANS OFF AND ANYTHING ELSE IS ON
 
-
 package org.usfirst.frc.team4139.robot.CAN;
 
 import edu.wpi.first.wpilibj.AnalogOutput;
-import com.ctre.*;
 
 public class CANDumper
 {
 	private CANTalon motor;
 	private boolean shouldMove, isUp;
-	
-	private AnalogOutput downSwitch;
-	private AnalogOutput upSwitch;
+	private AnalogOutput upSwitch, downSwitch;
 	
 	public CANDumper()
 	{
 		motor = new CANTalon(9);
-		motor.changeControlMode(CANTalon.TalonControlMode.PercentVbus); 
-		
+		motor.changeControlMode(CANTalon.TalonControlMode.PercentVbus); //stolen from climber class
 		shouldMove = false;
 		isUp = false;
-		
 		downSwitch = new AnalogOutput(7);
 		upSwitch = new AnalogOutput(6);
 	}
 	
-	/**
-	 * should be called whenever the dumper button is pressed in the Robot.java class
-	 */
+	//accessor methods for the instance fields
+	
+	public CANTalon getMotor()
+	{
+		return motor;
+	}
+	
+	public boolean getIsUp()
+	{
+		return isUp;
+	}
+
+	public boolean getShouldMove()
+	{
+		return shouldMove;
+	}
+	
+	public AnalogOutput getDownSwitch()
+	{
+		return downSwitch;
+	}
+	
+	public AnalogOutput getUpSwitch()
+	{
+		return upSwitch;
+	}
+	
+	
+	//should be called whenever the dumper button is pressed in the Robot.java class
 	public void activate()
 	{
 		shouldMove = true;
+		
+		System.out.println("Motor should move");
 	}
 	
-	/**
-	 * should be called with every call to teleopPeriodic in the Robot.java class
-	 */
-	public void update()
+	//should be called with every call to teleopPeriodic() in the Robot.java class
+	public boolean update()
 	{
-		if (isUpSwitchPressed())
+		if (upSwitch.getVoltage() > 0.1 || upSwitch.getVoltage() < -0.1)
 		{
 			shouldMove = false;
 			isUp = true;
-		} else if (downSwitch.getVoltage() != 0)
+			
+			System.out.println("upSwitch pressed");
+		}
+		
+		else if (downSwitch.getVoltage() > 0.1 || downSwitch.getVoltage() < -0.1)
 		{
 			shouldMove = false;
 			isUp = false;
+			
+			System.out.println("downSwitch pressed");
 		}
 		
 		if (shouldMove)
 		{
 			if (isUp)
 				motor.set(-0.5); //half speed
+			
 			else
 				motor.set(0.5);
 		}
 	}
-	
-	public boolean isUpSwitchPressed(){
-		return upSwitch.getVoltage() != 0;
-	}
 }
 
-
-
-/** Things to ask Kento
+/* Things to ask Kento
  * channels for both AnalogOutputs - anything that is on the roborio
  * what does getVoltage() return? - zero or other numbers
  * does positive motor movement mean up or down? - up?
